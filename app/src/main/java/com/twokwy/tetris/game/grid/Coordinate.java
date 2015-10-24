97a2900b5a6187acea26336bb4f595644b934156
Anita Woodruff <anita@swiftkey.net>
2015-10-24 19:12:09 +0100

    Adding equals and hashcode to Coordinate

--- app/src/main/java/com/twokwy/tetris/game/grid/Coordinate.java
+++ app/src/main/java/com/twokwy/tetris/game/grid/Coordinate.java
@@ -19,4 +19,23 @@ public class Coordinate {
     public int getY() {
         return mY;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Coordinate that = (Coordinate) o;
+
+        if (mX != that.mX) return false;
+        return mY == that.mY;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = mX;
+        result = 31 * result + mY;
+        return result;
+    }
 }
