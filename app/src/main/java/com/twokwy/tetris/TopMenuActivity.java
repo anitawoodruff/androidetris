8c6d7d3c361bc62a6d4c00be97210c82efccacb4
Anita Woodruff <anita@swiftkey.net>
2016-03-13 14:41:06 +0000

    Adding crashlytics

--- app/src/main/java/com/twokwy/tetris/TopMenuActivity.java
+++ app/src/main/java/com/twokwy/tetris/TopMenuActivity.java
@@ -5,9 +5,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import com.crashlytics.android.Crashlytics;
 import com.firebase.client.Firebase;
 import com.twokwy.tetris.game.GamePlayActivity;
 import com.twokwy.tetris.scores.HighScoresActivity;
+import io.fabric.sdk.android.Fabric;
 
 
 public class TopMenuActivity extends Activity {
@@ -15,6 +17,7 @@ public class TopMenuActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         Firebase.setAndroidContext(this);
         setContentView(R.layout.activity_top_menu);
     }
