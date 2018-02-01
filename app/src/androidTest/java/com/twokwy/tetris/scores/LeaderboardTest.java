40c45f437cbdc86b62c8478968715b946bf7b262
Anita Woodruff <anita.woodruff@gmail.com>
2018-02-01 00:23:01 +0000

    working on mah tests - all borked

--- app/src/androidTest/java/com/twokwy/tetris/scores/LeaderboardTest.java
+++ app/src/androidTest/java/com/twokwy/tetris/scores/LeaderboardTest.java
@@ -1,5 +1,6 @@
 package com.twokwy.tetris.scores;
 
+import android.content.SharedPreferences;
 import android.support.test.runner.AndroidJUnit4;
 
 import org.junit.Before;
@@ -9,6 +10,8 @@ import org.junit.runner.RunWith;
 import static android.support.test.InstrumentationRegistry.getInstrumentation;
 import static com.twokwy.tetris.scores.HighScoresActivity.HIGH_SCORE_SHARED_PREFS;
 import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.junit.Assert.*;
 
 /**
@@ -22,8 +25,10 @@ public class LeaderboardTest {
 
     @Before
     public void setUp() throws Exception {
-        mLeaderboard = new Leaderboard(getInstrumentation().getTargetContext()
-                .getSharedPreferences(HIGH_SCORE_SHARED_PREFS, 0));
+        SharedPreferences sharedPreferences = getInstrumentation().getTargetContext()
+                .getSharedPreferences(HIGH_SCORE_SHARED_PREFS, 0);
+        sharedPreferences.edit().clear().commit();
+        mLeaderboard = new Leaderboard(sharedPreferences);
     }
 
     @Test
@@ -47,6 +52,7 @@ public class LeaderboardTest {
         }
         assertThat(mLeaderboard.addScore(0), is(false));
         assertThat(mLeaderboard.size(), is(MAX_SIZE));
+        assertThat(mLeaderboard.getHighScores(), contains(0, 0, 0, 0, 0));
     }
 
     @Test
@@ -59,6 +65,44 @@ public class LeaderboardTest {
         boolean scoreWasInserted = mLeaderboard.addScore(MAX_SIZE + 1);
         assertThat(scoreWasInserted, is(true));
         assertThat(mLeaderboard.size(), is(MAX_SIZE));
+        assertThat(mLeaderboard.getHighScores(), contains(6, 5, 4, 3, 2));
     }
 
+    @Test
+    public void qualifyDistinguishesScoresWhenLeaderboardFull() throws Exception {
+        for (int i = 1; i <= MAX_SIZE; i++) {
+            assertThat(mLeaderboard.addScore(0), is(true));
+            assertThat(mLeaderboard.size(), is(i));
+        }
+        assertThat(mLeaderboard.qualifies(1), is(true));
+        assertThat(mLeaderboard.qualifies(0), is(false));
+    }
+
+    @Test
+    public void multipleInstancesWithSamePrefsShareScores() throws Exception {
+        SharedPreferences sharedPreferences = getInstrumentation().getTargetContext()
+                .getSharedPreferences("shared", 0);
+        sharedPreferences.edit().clear().commit();
+        Leaderboard firstLeaderboard = new Leaderboard(sharedPreferences);
+        Leaderboard secondLeaderboard = new Leaderboard(sharedPreferences);
+        firstLeaderboard.addScore(1);
+        assertThat(secondLeaderboard.getHighScores(), contains(1));
+        assertThat(secondLeaderboard.size(), is(1));
+        firstLeaderboard.addScore(2);
+        firstLeaderboard.addScore(3);
+        firstLeaderboard.addScore(4);
+        firstLeaderboard.addScore(5);
+        assertThat(firstLeaderboard.getHighScores(), contains(5, 4, 3, 2, 1));
+        assertThat(secondLeaderboard.getHighScores(), contains(5, 4, 3, 2, 1));
+
+        assertThat(secondLeaderboard.qualifies(0), is(false));
+        assertThat(secondLeaderboard.addScore(0), is(false));
+
+        assertThat(secondLeaderboard.qualifies(6), is(true));
+        assertThat(secondLeaderboard.addScore(6), is(true));
+
+        assertThat(secondLeaderboard.getHighScores(), contains(6, 5, 4, 3, 2));
+        assertThat(firstLeaderboard.getHighScores(), contains(6, 5, 4, 3, 2));
+
+    }
 }
--- app/src/main/java/com/twokwy/tetris/scores/Leaderboard.java
+++ app/src/main/java/com/twokwy/tetris/scores/Leaderboard.java
@@ -2,8 +2,9 @@ package com.twokwy.tetris.scores;
 
 import android.content.SharedPreferences;
 
+import com.google.common.collect.ImmutableList;
+
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -13,19 +14,23 @@ import java.util.List;
 
 public class Leaderboard {
     private static final int MAX_SIZE = 5;
+    private static final String HIGH_SCORE_KEY_PREFIX = "high-score-number-";
     private final SharedPreferences mSharedPreferences;
     /**
-     * This is sorted from best to worst.
+     * This is sorted from worst to best.
      */
     private List<Integer> mScores;
 
     Leaderboard(SharedPreferences sharedPreferences) {
         mSharedPreferences = sharedPreferences;
-        mScores = new ArrayList<>(MAX_SIZE);
+        mScores = getScores();
     }
 
+    /**
+     * @return true if the score would be a new high score.
+     */
     boolean qualifies(int score) {
-        return true;
+        return !isFull() || score > lowestHighScore();
     }
 
     int maxSize() {
@@ -33,19 +38,63 @@ public class Leaderboard {
     }
 
     int size() {
-        return mScores.size();
+        return getScores().size();
     }
 
+    /**
+     * Inserts the score into the leaderboard if it qualifies as a new high score.
+     * Scores added earlier register higher in the leaderboard than equal scores added later.
+     * @return true if this was a new high score.
+     */
     boolean addScore(int score) {
-        boolean newHighScore = false;
-        if (mScores.size() < MAX_SIZE || score > mScores.get(mScores.size() - 1)) {
-            newHighScore = true;
+        if (!qualifies(score)) {
+            return false;
         }
+        mScores = getScores();
         mScores.add(score);
         Collections.sort(mScores);
-        if (mScores.size() > MAX_SIZE) {
-            mScores.subList(MAX_SIZE, mScores.size()).clear();
+        mScores.remove(0);
+        writeScoresToPrefs(ImmutableList.copyOf(mScores), mSharedPreferences);
+        return true;
+    }
+
+    private void writeScoresToPrefs(ImmutableList<Integer> scores,
+                                    SharedPreferences sharedPreferences) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        for (int i = 0; i < scores.size(); i++) {
+            editor.putInt(HIGH_SCORE_KEY_PREFIX + i, scores.get(i));
         }
-        return newHighScore;
+        for (int i = scores.size(); i < MAX_SIZE; i++) {
+            editor.remove(HIGH_SCORE_KEY_PREFIX + i);
+        }
+        editor.commit();
+    }
+
+    private Integer lowestHighScore() {
+        return getScores().get(0);
+    }
+
+    private boolean isFull() {
+        return getScores().size() == MAX_SIZE;
+    }
+
+    /**
+     * Gets any high scores in order of highest to lowest.
+     */
+    List<Integer> getHighScores() {
+        List<Integer> scoresToReturn = getScores();
+        Collections.reverse(scoresToReturn);
+        return scoresToReturn;
+    }
+
+    private List<Integer> getScores() {
+        List<Integer> scoresToReturn = new ArrayList<>();
+        for (int i = 0; i < MAX_SIZE; i++) {
+            if (!mSharedPreferences.contains(HIGH_SCORE_KEY_PREFIX + i)) {
+                break;
+            }
+            scoresToReturn.add(mSharedPreferences.getInt(HIGH_SCORE_KEY_PREFIX + i, -1));
+        }
+        return scoresToReturn;
     }
 }
